554a4d454243861414cf63c938852c46b432afd1
zfriedman0 <friedman1080@outlook.com>
2024-08-08 15:57:57 -0500

    Updated '66. Plus One' solution.

--- easy/plusOne.py
+++ easy/plusOne.py
@@ -1,2 +1,14 @@
+'''
+66. Plus One
+
+You are given a large integer represented as an integer array `digits`, where each `digits[i]` is the `ith` digit of the integer. The digits are ordered from most significant to least significant in left-to-right order. The large integer does not contain any leading `0`s.
+
+Increment the large integer by one and return the resulting array of digits.
+'''
 def plusOne(digits):
-    return int("".join)
+    num = int("".join(str(digit) for digit in digits)) + 1
+    result = [int(digit) for digit in str(num)]
+
+    return result
+
+print(plusOne([1,2,3]))
